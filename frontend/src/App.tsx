e1118d75df166bd00a011a12bdc160dc476a6002
Thu Nguyen <101914949+thu-ngx@users.noreply.github.com>
2026-01-25 11:02:33 +0700

    (FE) add routes, install shadcn and add color config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
 
+function App() {
   return (
     <>
-      hello
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* public routes */}
+          <Route path="/signup" element={<SignUpPage />}></Route>
+          <Route path="/login" element={<LogInPage />}></Route>
+          {/* protected routes */}
+          {/* TODO: create protected route */}
+          <Route path="/" element={<ChatAppPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
